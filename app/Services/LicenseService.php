933e89b4e31bd81a64396b1635526f04d6acee04
rain07-f <reyno.fahreza@gmail.com>
2026-02-26 15:18:21 +0700

    fix-license

--- app/Services/LicenseService.php
+++ app/Services/LicenseService.php
@@ -56,7 +56,7 @@ class LicenseService
         for ($i = 0; $i < 4; $i++) {
             $parts[] = strtoupper(Str::random(4));
         }
-        return 'DCTL-' . implode('-', $parts);
+        return 'LIC07-' . implode('-', $parts);
     }
 
     /**
